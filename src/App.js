fda71c12baeec408299f149afd89be5f143fb9e1
Luis Gil <luis.gil.g@gmail.com>
2022-09-08 15:16:53 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coming soon...
+        </p>
+        <a
+          className="App-link"
+          href="https://opensea.io/collection/oblokos"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Opensea collection
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
